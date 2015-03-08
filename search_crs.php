a1b5a97c5ffaf08e462d627fc570007eeeb881d3
MOLEDesign <Development@moledesign.biz>
2015-03-08 23:19:57 +0000

    New framework

---
+++ search_crs.php
@@ -0,0 +1,23 @@
+<?php
+  require("OpenLDBWS.php");
+
+  $OpenLDBWS = new OpenLDBWS("f37ace7b-1a82-4833-9908-48a3749dc17a");
+
+if (!isset($_REQUEST['crs'])) {
+    echo 'Please supply a station';
+    return;
+}
+
+
+    $crs = $_REQUEST['crs'];
+    $crs = strtoupper($crs);
+
+    $departureBoard = $OpenLDBWS->GetDepartureBoard(30,$crs);
+
+    header("Content-Type: text/plain");
+
+    // JSONify the string and return it
+    $returnedJSON = json_encode($departureBoard);
+    echo $returnedJSON;
+
+?>
